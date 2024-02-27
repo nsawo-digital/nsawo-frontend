141713aefc8af2f7c4f9b6064d145ddfbb84e6e1
othie12 <simonothieno10@gmail.com>
2024-02-27 22:57:28 +0300

    modify cookie fetcher

--- utils/axiosClientCustomizer.ts
+++ utils/axiosClientCustomizer.ts
@@ -1,12 +1,11 @@
 'use client'
-import Cookies from "js-cookie";
+
 import axios from "axios";
 
-const token = Cookies.get('access_token')
 export const apiClient = axios.create({
     baseURL: 'http://localhost:3001',
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${token || ''}`,
+      Authorization: `Bearer `,
     },
   })
